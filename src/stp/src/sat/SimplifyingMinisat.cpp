da3f45cf27bf8e112103a047c143e1593c30404a
Brian Foley <bpfoley@google.com>
2020-02-04 20:51:48 -0800

    Put spaces before/after PRI macros to GCC's -Wliteral-suffix
    GCC warns about this because C++11 adds user defined literals
    operator"". See https://en.cppreference.com/w/cpp/language/user_literal

--- src/stp/src/sat/SimplifyingMinisat.cpp
+++ src/stp/src/sat/SimplifyingMinisat.cpp
@@ -70,11 +70,11 @@ namespace BEEV
   {
     double cpu_time = Minisat::cpuTime();
     double mem_used = Minisat::memUsedPeak();
-    printf("restarts              : %"PRIu64"\n", s->starts);
-    printf("conflicts             : %-12"PRIu64"   (%.0f /sec)\n", s->conflicts   , s->conflicts   /cpu_time);
-    printf("decisions             : %-12"PRIu64"   (%4.2f %% random) (%.0f /sec)\n", s->decisions, (float)s->rnd_decisions*100 / (float)s->decisions, s->decisions   /cpu_time);
-    printf("propagations          : %-12"PRIu64"   (%.0f /sec)\n", s->propagations, s->propagations/cpu_time);
-    printf("conflict literals     : %-12"PRIu64"   (%4.2f %% deleted)\n", s->tot_literals, (s->max_literals - s->tot_literals)*100 / (double)s->max_literals);
+    printf("restarts              : %" PRIu64 "\n", s->starts);
+    printf("conflicts             : %-12" PRIu64 "   (%.0f /sec)\n", s->conflicts   , s->conflicts   /cpu_time);
+    printf("decisions             : %-12" PRIu64 "   (%4.2f %% random) (%.0f /sec)\n", s->decisions, (float)s->rnd_decisions*100 / (float)s->decisions, s->decisions   /cpu_time);
+    printf("propagations          : %-12" PRIu64 "   (%.0f /sec)\n", s->propagations, s->propagations/cpu_time);
+    printf("conflict literals     : %-12" PRIu64 "   (%4.2f %% deleted)\n", s->tot_literals, (s->max_literals - s->tot_literals)*100 / (double)s->max_literals);
     if (mem_used != 0) printf("Memory used           : %.2f MB\n", mem_used);
     printf("CPU time              : %g s\n", cpu_time);
   }
