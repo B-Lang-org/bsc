25da4c52cf3cb8290f6ef095ea74037063ac90a6
Julie Schwartz <quark@bluespec.com>
2020-02-09 23:03:19 -0500

    Allow Yices to be used as the solver in type checking
    Now, all places that use an SMT solver can respect the choice of solver
    according to the flags.

--- src/comp/SATPred.hs
+++ src/comp/SATPred.hs
@@ -9,10 +9,8 @@ import Pred
 
 import qualified Pred2STP as STP
          (SState, initSState, solvePred)
-{-
 import qualified Pred2Yices as Yices
          (YState, initYState, solvePred)
--}
 
 -- -------------------------
 
@@ -20,18 +18,12 @@ import qualified Pred2Yices as Yices
 -- For now, we always use STP for this
 data SATPredState =
            SATPredS_STP STP.SState
-{-
          | SATPredS_Yices Yices.YState
-         | SATPredS_Unimp
--}
 
 -- -------------------------
 
 initSATPredState :: Flags -> IO SATPredState
 initSATPredState flags = do
-    stp_state <- STP.initSState
-    return (SATPredS_STP stp_state)
-{-
     case (satBackend flags) of
       SAT_STP -> do
         stp_state <- STP.initSState
@@ -39,7 +31,6 @@ initSATPredState flags = do
       SAT_Yices -> do
         yices_state <- Yices.initYState
         return (SATPredS_Yices yices_state)
--}
 
 -- -------------------------
 
@@ -48,13 +39,9 @@ checkPreds :: SATPredState -> [Pred] -> IO ([EMsg], SATPredState)
 checkPreds (SATPredS_STP stp_state) ps = do
     (res, stp_state') <- STP.checkPreds stp_state ps
     return (res, SATPredS_STP stp_state')
-{-
 checkPreds (SATPredS_Yices yices_state) ps = do
     (res, yices_state') <- Yices.checkPreds yices_state ps
     return (res, SATPredS_Yices yices_state')
-checkPreds SATPredS_Unimp _ = do
-    return ([], SATPredS_Unimp)
--}
 -}
 
 -- -------------------------
@@ -63,13 +50,9 @@ solvePred :: SATPredState -> [Pred] -> Pred -> IO (Maybe Pred, SATPredState)
 solvePred (SATPredS_STP stp_state) ps p = do
     (res, stp_state') <- STP.solvePred stp_state ps p
     return (res, SATPredS_STP stp_state')
-{-
 solvePred (SATPredS_Yices yices_state) ps p = do
     (res, yices_state') <- Yices.solvePred yices_state ps p
     return (res, SATPredS_Yices yices_state')
-solvePred SATPredS_Unimp _ _ = do
-    return (Right Nothing, SATPredS_Unimp)
--}
 
 -- -------------------------
 
