43c54fd03d3fb9e96e748691e76e8d117fdd04aa
Brian Foley <bpfoley@google.com>
2020-02-20 17:40:03 -0800

    Delete vendored copies of TCL, Tk, itcl, itk

--- src/tcltk/tcltk8.5.4/tcl8.5.4/library/http/pkgIndex.tcl
+++
@@ -1,4 +0,0 @@
-# Tcl package index file, version 1.1
-
-if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded http 2.7.1 [list tclPkgSetup $dir http 2.7.1 {{http.tcl source {::http::config ::http::formatQuery ::http::geturl ::http::reset ::http::wait ::http::register ::http::unregister ::http::mapReply}}}]
--- src/tcltk/tcltk8.5.4/tcl8.5.4/library/msgcat/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-if {![package vsatisfies [package provide Tcl] 8.5]} {return}
-package ifneeded msgcat 1.4.2 [list source [file join $dir msgcat.tcl]]
