74211cd99b4dcbfdceae69b3c6eaf865c9d819e1
Brian Foley <bpfoley@google.com>
2020-03-01 13:54:26 -0800

    Update build to work with itcl 3 or 4.
    Unfortunately the package naming convention changed for itcl 4, so
    we have to accomodate this.
    
    Update the ubuntu build scripts to pick the latest version of the
    libraries available for the given OS.

--- .github/workflows/install_dependencies_ubuntu.sh
+++ .github/workflows/install_dependencies_ubuntu.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 sudo apt-get update
+
+# Install the latest version of itcl/itk -- itk3-dev or tk-itk4-dev.
+# NB itk depends on itcl, tk and tcl, so no need to install those separately
+ITK_DEV_PKG=$(apt-cache search -n '^(tk-)?itk[0-9]-dev' | cut -f1 -d' ')
+
 sudo apt-get install -y \
   autoconf \
   bison \
@@ -9,10 +14,7 @@ sudo apt-get install -y \
   git \
   gperf \
   iverilog \
-  tcl-dev \
-  tk-dev \
-  itcl3-dev \
-  itk3-dev \
+  $ITK_DEV_PKG \
   libfontconfig1-dev \
   libghc-old-time-dev \
   libghc-regex-compat-dev \
