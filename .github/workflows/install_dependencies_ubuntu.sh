262df967532107dc61d9cd8f36d6a3d48cc71e10
Paulo Matias <matias@ufscar.br>
2020-04-11 20:48:08 -0300

    Add Dockerfile

--- .github/workflows/install_dependencies_ubuntu.sh
+++ .github/workflows/install_dependencies_ubuntu.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-sudo apt-get update
+apt-get update
 
 # Install the latest version of itcl/itk -- itk3-dev or tk-itk4-dev.
 # NB itk depends on itcl, tk and tcl, so no need to install those separately
 ITK_DEV_PKG=$(apt-cache search -n '^(tk-)?itk[0-9]-dev' | cut -f1 -d' ' | sort | tail -1)
 
-sudo apt-get install -y \
+apt-get install -y \
   autoconf \
   bison \
   build-essential \
