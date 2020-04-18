3a3d2665ef1f2818db23a1bc8030fe20557881d1
Brian Foley <bpfoley@google.com>
2020-04-17 17:19:52 -0700

    Remove bluewish, and all dependencies (including tk, x11, fontconfig)
    Update the docs to reflect this, and while here, update a slightly
    bitrotted vagrant provisioning script.
    
    This resolves issue #116 by removing the now-superfluous bluewish.

--- .github/workflows/install_dependencies_macos.sh
+++ .github/workflows/install_dependencies_macos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-brew install autoconf cabal-install fontconfig gperf icarus-verilog pkg-config && \
+brew install autoconf cabal-install gperf icarus-verilog pkg-config && \
     cabal update && \
     cabal v1-install old-time regex-compat split syb
