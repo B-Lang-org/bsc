9f4a447f8fafe0eec4200c5495e7df14fb2af88d
Brian Foley <bpfoley@google.com>
2020-05-05 20:36:34 -0700

    Use ccache to cache C++ compilation across builds
    ccache will use hashes of the full compiler command line and input
    files to determine whether to use previously compiled object files
    or not, so this is safe.
    
    Cumulatively before caching stp and yices take about 2.5mins to
    compile on Ubuntu.
    
    Store an updated cache after every build, and rely on github's cache
    pruning to remove the oldest caches.
    
    Currently <100MB of uncompressed object files per OS.

--- .github/workflows/install_dependencies_macos.sh
+++ .github/workflows/install_dependencies_macos.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-brew install autoconf cabal-install gperf icarus-verilog pkg-config && \
+# ccache is not required to buid bsc, but we use it in build.yml to improve
+# the build performance by caching C++ obj files across multiple builds.
+brew install ccache autoconf cabal-install gperf icarus-verilog pkg-config && \
     cabal update && \
     cabal v1-install old-time regex-compat split syb
--- .github/workflows/install_dependencies_ubuntu.sh
+++ .github/workflows/install_dependencies_ubuntu.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 apt-get update
 
+# ccache is not required to buid bsc, but we use it in build.yml to improve
+# the build performance by caching C++ obj files across multiple builds.
 apt-get install -y \
+  ccache \
   autoconf \
   bison \
   build-essential \
