dd43a02ace97861d81b8eee0fc2f2a6769e4b9c9
Lucas Kramer <lkramer@google.com>
2020-12-04 00:19:09 +0000

    Generate a Generic instance for generated wrapper structs that uses the original interface as the representation

--- src/comp/GenWrap.hs
+++ src/comp/GenWrap.hs
@@ -348,10 +348,15 @@ genWrapE generating ppmap cpack@(CPackage packageId exps imps fixs ds includes) 
 
        let fixedDefs = map fixDef defsWithVector
 
+       instanceDefs <- concatMapM mkInstances trs
+       -- XXX we don't update the symbol table with the new instances
+       -- XXX we rely on the symbol table being rebuilt
+
        let finalDefs = reverse fixedDefs ++
                        ifcdefns ++
                        newModule_s ++
-                       ifcConversionDefs
+                       ifcConversionDefs ++
+                       instanceDefs
 
        gens <- mapM (genWrapInfo newFlatIfcs) moduledefs
 
@@ -1250,6 +1255,82 @@ genFrom pps ty var =
                               else return (CApply eUnpack [ec], [r])
               let ctxs = nub (res_ctxs ++ as)
               return (f, vs, e, qs, ctxs)
+
+
+-- --------------------
+-- genWrapE toplevel: mkInstances
+
+-- The Generic representation of a wrapper type is the type being wrapped -
+-- here we derive a Generic instance which uses the
+-- "fromIfc_" function and calls the class method for the original interface.
+
+mkInstances :: IfcTRec -> GWMonad [CDefn]
+mkInstances trec = do
+  genericInstance <- mkGeneric trec
+  return [genericInstance]
+
+mkGeneric :: IfcTRec -> GWMonad CDefn
+mkGeneric (IfcTRec { rec_id = flat_id, rec_type = orig_ty }) = do
+  ctxs <- mkCtxs orig_ty
+  let flat_ty = cTCon flat_id
+      cqt = CQType ctxs (cTApplys (cTCon idGeneric) [flat_ty, TAp (cTCon idConc) orig_ty])
+      defn = Cinstance cqt
+        [CLValue (unQualId idFrom)
+          [CClause [CPVar id_x] [] $
+            CCon idConc [cVApply (from_Id flat_id) [CVar id_x]]] [],
+         CLValue (unQualId idTo)
+          [CClause [CPCon idConc [CPVar id_x]] [] $
+            cVApply (to_Id flat_id) [CVar id_x]] []]
+  return defn
+
+-- XXX this duplicates work done elsewhere; just do one traversal and
+-- XXX reuse it in genIfcField / mkTo / mkFrom / mkFromBind
+mkCtxs :: CType -> GWMonad [CPred]
+mkCtxs ty =
+ do
+   cint <- chkInterface ty
+   case cint of
+     Nothing -> internalError ("genFrom: " ++ pfpReadable ty)
+     Just (_, _, fts) -> do
+       css <- mapM meth fts
+       let bits_types = unions css
+           ctxs = [ CPred (CTypeclass idBits) [t, cTVarNum v]
+                        | (t, v) <- zip bits_types tmpTyVarIds ]
+       return ctxs
+ where
+   meth :: FInf -> GWMonad [CType]
+   meth (FInf _ as r _) =
+    do
+      mi <- chkInterface r
+      case (mi, as) of
+        (Just (_, _, fts), []) -> do
+          ctxss <- mapM meth fts
+          return (unions ctxss)
+        _ -> do
+          isVec <- isVectorInterfaces r
+          case (isVec, as) of
+            (Just (n, tVec, isListN), []) ->
+               meth (FInf (mkNumId 0) [] tVec [])
+            _ -> do
+              isPA <- isPrimAction r
+              isClock <- isClockType r
+              isReset <- isResetType r
+              isInout <- isInoutType r
+              isAV <- isActionValue r
+              res_ctxs <-
+                  case isInout of
+                    Just iot -> return [iot]
+                    _ -> if (isPA || isClock || isReset)
+                          then return []
+                          else
+                           if isAV
+                              then do
+                                retType <- getAVType "genFrom" r
+                                return [retType]
+                              else return [r]
+              let ctxs = nub (res_ctxs ++ as)
+              return (ctxs)
+
 
 -- --------------------
 -- genWrapE toplevel: mkNewModDef
