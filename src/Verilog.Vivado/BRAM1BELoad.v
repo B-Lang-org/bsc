105ebe864c6c9236e050f3830795cef19fccdbf2
Austin Seipp <aseipp@pobox.com>
2021-01-16 03:18:49 -0600

    prims: remove deprecated Icarus Verilog workarounds
    Icarus seems to support the 'generate' constructs used here for BRAMs,
    so there's no need to use this workaround anymore.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/Verilog.Vivado/BRAM1BELoad.v
+++ src/Verilog.Vivado/BRAM1BELoad.v
@@ -53,42 +53,6 @@ module BRAM1BELoad(CLK,
         $readmemh(FILENAME, RAM, 0, MEMSIZE-1);
    end
 
-   // iverilog does not support the full verilog-2001 language.  This fixes that for simulation.
-`ifdef __ICARUS__
-   reg [DATA_WIDTH-1:0]  MASK, IMASK;
-   reg [DATA_WIDTH-1:0]  DATA;
-   wire [DATA_WIDTH-1:0] DATAwr;
-
-   assign DATAwr = RAM[ADDR] ;
-
-
-   always @(WE or DI or DATAwr) begin : combo1
-      integer j;
-      MASK  = 0;
-      IMASK = 0;
-
-      for(j = WE_WIDTH-1; j >= 0; j = j - 1) begin
-         if (WE[j]) MASK = (MASK << 8) | { { DATA_WIDTH-CHUNKSIZE { 1'b0 } }, { CHUNKSIZE { 1'b1 } } };
-         else       MASK = (MASK << 8);
-      end
-      IMASK = ~MASK;
-
-      DATA = (DATAwr & IMASK) | (DI & MASK);
-   end
-
-   
-   always @(posedge CLK) begin
-      if (EN) begin
-         if (WE) begin
-            RAM[ADDR] <= `BSV_ASSIGNMENT_DELAY DATA;
-            DO_R      <= `BSV_ASSIGNMENT_DELAY DATA;
-         end
-         else begin
-            DO_R      <= `BSV_ASSIGNMENT_DELAY RAM[ADDR];
-         end
-      end
-   end
-`else
    generate
       genvar i;
       for(i = 0; i < WE_WIDTH; i = i + 1) begin: porta_we
@@ -105,7 +69,6 @@ module BRAM1BELoad(CLK,
          end
       end      
    endgenerate
-`endif // !`ifdef __ICARUS__
 
    // Output driver
    always @(posedge CLK) begin
