cee78fdd3ba8f88856e12eb7b7bd67d7f250ab6d
Julie Schwartz <quark@bluespec.com>
2021-01-27 12:43:56 +1300

    Initial release of BSC doc sources

---
+++ .github/workflows/install_dependencies_doc_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+apt-get update
+
+apt-get install -y \
+  texlive-latex-base \
+  texlive-latex-recommended \
+  texlive-latex-extra \
+  texlive-font-utils \
+  texlive-fonts-extra
