e2216f91994fb80fd56dad43a7908c2ac2f4c154
Julie Schwartz <quark@bluespec.com>
2021-07-27 15:02:54 +1200

    Remove Ubuntu 16.04 from CI jobs
    This environment has been deprecated and will be removed 20 Sept 2021.
    See:  https://github.com/actions/virtual-environments/issues/3287

--- .github/workflows/install_dependencies_testsuite_ubuntu.sh
+++ .github/workflows/install_dependencies_testsuite_ubuntu.sh
@@ -5,15 +5,12 @@ apt-get update
 apt-get install -y \
     ccache \
     build-essential \
+    lld \
     tcsh \
     dejagnu \
     iverilog
 
 REL=$(lsb_release -rs | tr -d .)
-if [ $REL -ge 1804 ]; then
-    apt-get install -y lld
-fi
-
 if [ $REL -ge 1910 ]; then
     apt-get install -y libsystemc-dev
 fi
