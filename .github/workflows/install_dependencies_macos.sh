6ae5d47fec9b60078abd395f0c1abf8eddc798eb
Julie Schwartz <quark@bluespec.com>
2021-09-24 09:14:52 +1200

    Hold back ccache on macOS to avoid bug in 4.4

--- .github/workflows/install_dependencies_macos.sh
+++ .github/workflows/install_dependencies_macos.sh
@@ -3,8 +3,13 @@
 # ccache is not required to build bsc, but we use it in build.yml to improve
 # the build performance by caching C++ obj files across multiple builds.
 brew install \
-  ccache \
   autoconf \
   gperf \
   icarus-verilog \
   pkg-config
+
+# Hold ccache back to 4.3_1, to avoid bug introduced in 4.4
+TAP=b-lang/homebrew-old
+brew tap-new $TAP
+brew extract --version 4.3 homebrew/core/ccache $TAP
+brew install $TAP/ccache@4.3
