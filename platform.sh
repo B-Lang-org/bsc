B-Lang-org/bsc

commit 5f987061aa218d41381d5bf00252517283714dba

Enable building on FreeBSD
Move the computation of the TCL_INC_FLAGS and TCL_LIB_FLAGS from two
places in the build to one place, in the top-level platform script.
Expand that computation to include more cases.

The existing build system conflated 'has BSD tools' vs 'has GNU tools',
'is ELF' vs 'is Mach-O', and 'is Linux' vs 'is Darwin'.  I have not tried
to clean up that conflation, only to add the missing case.

Includes reviewer feedback.

platform.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -u
+
 usage()
 {
     echo "Usage: $0 <option>"
@@ -8,6 +10,9 @@ usage()
     echo "Valid options are:"
     echo "   ostype   "
     echo "   machtype "
+    echo "   tclsh "
+    echo "   tclinc "
+    echo "   tcllibs "
     echo "   c++_shared_flags"
 }
 
@@ -19,11 +24,8 @@ fi
 ## =========================
 ## OSTYPE
 
-if [ -z "${OSTYPE}" ] ; then
-    OSTYPE=`uname -s`
-fi
 ## Account for values like "linux-gnu" by removing extra fields
-OSTYPE=$(echo ${OSTYPE} | cut -d'-' -f1)
+OSTYPE=$(echo ${OSTYPE:=$(uname -s)} | cut -d'-' -f1)
 ## Account for values like "Darwin10.0" by removing the version number
 OSTYPE=$(echo ${OSTYPE} | egrep -o "^[A-Za-z]+")
 ## Account for lowercase values like "linux" when we want "Linux"
@@ -37,12 +39,15 @@ fi
 ## =========================
 ## MACHTYPE
 
-if [ -z "${MACHTYPE}" ] ; then
-    MACHTYPE=`uname -m`
-fi
 ## Account for values like "x86_64-pc-linux-gnu" when all we want is
 ## "x86_64", "i386", etc
-MACHTYPE=$(echo ${MACHTYPE} | cut -d'-' -f1)
+MACHTYPE=$(echo ${MACHTYPE:=$(uname -m)} | cut -d'-' -f1)
+
+# If we see a BSD-style amd64 instead of x86_64, rewrite it to the more generic
+# version.
+if [ ${MACHTYPE} = "amd64" ] ; then
+    MACHTYPE=x86_64
+fi
 
 if [ "$1" = "machtype" ] ; then
     echo ${MACHTYPE}
@@ -63,6 +68,105 @@ if [ "$1" = "c++_shared_flags" ] ; then
 fi
 
 ## =========================
+## Find the TCL shell command
+
+if [ ${OSTYPE} = "Darwin" ] ; then
+    # Have Makefile avoid Homebrew's install of tcl on Mac
+    TCLSH=/usr/bin/tclsh
+else
+    TCLSH=`which tclsh`
+fi
+
+if [ "$1" = "tclsh" ] ; then
+    echo ${TCLSH}
+    exit 0
+fi
+
+PKG_CONFIG=`which pkg-config`
+if [ -z "${PKG_CONFIG}" ] ; then
+	PKG_CONFIG='false'
+fi
+
+TCL_SUFFIX=$(echo 'catch { puts [info tclversion]; exit 0}; exit 1' | ${TCLSH})
+TCL_ALT_SUFFIX=$(echo ${TCL_SUFFIX} | sed 's/\.//')
+
+if [ "$1" = "tclinc" ] ; then
+    # Avoid Homebrew's install of Tcl on Mac
+    if [ ${OSTYPE} = "Darwin" ] ; then
+	# no flags needed
+	exit 0
+    fi
+
+    # Try pkg-config
+    TCL_INC_FLAGS=$(${PKG_CONFIG} --silence-errors --cflags-only-I tcl${TCL_SUFFIX})
+    # If pkg-config didn't work with the first prefix, try the alternative version.
+    # For example, on FreeBSD, the tcl87 package installs tclsh8.7, but tcl87.pc
+    if [ $? -ne 0 ] ; then
+        TCL_INC_FLAGS=$(${PKG_CONFIG} --silence-errors --cflags-only-I tcl${TCL_ALT_SUFFIX})
+    fi
+    # If pkg-config doesn't work, try some well-known locations
+    if [ $? -ne 0 ] ; then
+        if [ -f "/usr/local/include/tcl${TCL_SUFFIX}/tcl.h" ] ; then
+	    TCL_INC_FLAGS="-I/usr/local/include/tcl${TCL_SUFFIX}"
+        elif [ -f "/usr/include/tcl${TCL_SUFFIX}/tcl.h" ] ; then
+	    TCL_INC_FLAGS="-I/usr/include/tcl${TCL_SUFFIX}"
+        else
+	    exit 1
+        fi
+    fi
+    echo ${TCL_INC_FLAGS}
+    exit 0
+fi
+
+if [ "${OSTYPE}" = "Darwin" ] ; then
+    LIB_SUFFIX=dylib
+else
+    LIB_SUFFIX=so
+fi
+
+if [ "$1" = "tcllibs" ] ; then
+    TCL_VER=`echo 'catch { puts [info tclversion]; exit 0}; exit 1' | ${TCLSH}`
+
+    # Avoid Homebrew's install of Tcl on Mac
+    if [ ${OSTYPE} = "Darwin" ] ; then
+	echo -ltcl$(TCL_VER)
+	exit 0
+    fi
+
+    # Try pkg-config
+    TCL_LIB_FLAGS=`${PKG_CONFIG} --silence-errors --libs tcl${TCL_SUFFIX}`
+    # If pkg-config didn't work with the first prefix, try the alternative version.
+    # For example, on FreeBSD, the tcl87 package installs tclsh8.7, but tcl87.pc
+    if [ $? -ne 0 ] ; then
+        TCL_LIB_FLAGS=`${PKG_CONFIG} --silence-errors --libs tcl${TCL_ALT_SUFFIX}`
+    fi
+
+    if [ $? -eq 0 ] ; then
+        echo ${TCL_LIB_FLAGS}
+        exit 0
+    fi
+
+    # If pkg-config doesn't work, try some well-known locations
+    for L in /usr/lib /usr/local/lib ; do
+        for V in ${TCL_VER} ${TCL_SUFFIX} ${TCL_ALT_SUFFIX} ; do
+            if [ -f "${L}/libtcl${V}.${LIB_SUFFIX}" ] ; then
+                echo -L${L} -ltcl${V}
+                exit 0
+            fi
+        done
+    done
+    # If we're Linux, look for multiarch things
+    if [ "${OSTYPE}" = "Linux" ] ; then
+        for V in ${TCL_VER} ${TCL_SUFFIX} ${TCL_ALT_SUFFIX} ; do
+            if [ -f "/usr/lib/${MACHTYPE}-linux-gnu/libtcl${V}.${LIB_SUFFIX}" ] ; then
+                echo -ltcl${V}
+                exit 0
+            fi
+        done
+    fi
+    exit 1
+fi
+
 
 usage
 exit 1

platform.mk
@@ -29,7 +29,7 @@ endif
 OSTYPE = $(shell $(TOP)/platform.sh ostype)
 export OSTYPE
 
-ifneq ($(OSTYPE), $(findstring $(OSTYPE), Linux Darwin))
+ifneq ($(OSTYPE), $(findstring $(OSTYPE), Linux Darwin Freebsd))
 $(error OSTYPE environment not recognized: $(OSTYPE))
 endif
 
@@ -56,3 +56,26 @@ export CFLAGS
 export CXXFLAGS
 
 ## --------------------
+## Set up the TCL shell and include paths
+
+ifneq ($(WANT_TCL),)
+
+TCLSH = $(shell $(TOP)/platform.sh tclsh)
+ifeq ($(TCLSH), )
+$(error Unable to find tclsh)
+endif
+$(info Using tclsh: $(TCLSH))
+
+TCL_INC_FLAGS = $(shell $(TOP)/platform.sh tclinc 2> /dev/null || echo fail)
+ifeq ($(TCL_INC_FLAGS), fail)
+$(error Unable to find tcl include directory)
+endif
+$(info Using tcl include flags: $(TCL_INC_FLAGS))
+
+TCL_LIB_FLAGS = $(shell $(TOP)/platform.sh tcllibs 2> /dev/null || echo fail)
+ifeq ($(TCL_LIB_FLAGS), fail)
+$(error Unable to find tcl library flags)
+endif
+$(info Using tcl library flags: $(TCL_LIB_FLAGS))
+
+endif

src/Makefile
@@ -8,13 +8,11 @@ PREFIX?=$(TOP)/inst
 ifndef NO_DEPS_CHECKS
 CC_TOOLS=$(CC) $(CXX) $(LD)
 BSC_TOOLS=ghc ghc-pkg
-BLUETCL_TOOLS=tclsh
 YICES_TOOLS=gperf autoconf
 STP_TOOLS=flex bison
 
 TOOLS=$(CC_TOOLS) \
       $(BSC_TOOLS) \
-      $(BLUETCL_TOOLS) \
       $(YICES_TOOLS) \
       $(STP_TOOLS) \
 

src/bluesim/Makefile
@@ -37,8 +37,8 @@ HEADERS = bluesim_kernel_api.h \
           bs_model.h
 
 # These ld export maps get copies to the inst/lib/Bluesim area
-LINKFILES = bs_linux_export_map.txt \
-            bs_darwin_export_map.txt
+LINKFILES = bs_elf_export_map.txt \
+            bs_mach-o_export_map.txt
 
 # -------------------------
 

src/bluetcl/Makefile
@@ -4,6 +4,8 @@ TOP:=$(PWD)/../..
 PREFIX?=$(TOP)/inst
 INSTALLDIR = $(PREFIX)/lib/tcllib/bluespec
 
+TCLSH = $(shell $(TOP)/platform.sh tclsh)
+
 FILES = \
 	tclIndex \
 	pkgIndex.tcl \
@@ -26,10 +28,8 @@ EXEFILES = \
 .PHONY: all
 all: tclIndex
 
-# pkg_mkIndex.tcl is produced here
-.PHONY: tclIndex
 tclIndex: *.tcl
-	./tclIndex.sh "$(TCLFILES)" "$(PACKAGES)"
+	${TCLSH} ./tclIndex.tcl "$(TCLFILES)" "$(PACKAGES)"
 
 .PHONY: install
 install: tclIndex

src/bluetcl/tclIndex.tcl
@@ -1,8 +1,3 @@
-#!/bin/sh
-
-#\
-exec tclsh "$0" "$@"
-
 set tclFiles [lindex $argv 0]
 set packFiles [lindex $argv 1]
 

src/comp/Makefile
@@ -1,5 +1,5 @@
-PWD:=$(shell pwd)
-TOP:=$(PWD)/../..
+TOP:=$(shell pwd)/../..
+WANT_TCL=yes
 
 include $(TOP)/platform.mk
 
@@ -59,27 +59,6 @@ YICES_LIB_FLAGS = -L../vendor/yices/lib -lyices
 HTCL_HS = ../vendor/htcl
 HTCL_INC_FLAGS = -L$(HTCL_HS)
 HTCL_LIB_FLAGS = -lhtcl
-
-# TCL version
-ifeq ($(OSTYPE), Darwin)
-# Have Makefile avoid Homebrew's install of tcl on Mac
-TCLSH=/usr/bin/tclsh
-else
-TCLSH=tclsh
-endif
-TCL_VER = $(shell echo 'catch { puts [info tclversion]; exit 0}; exit 1' | $(TCLSH) || echo fail)
-ifeq ($(TCL_VER),fail)
-$(error TCL version check failed)
-endif
-
-# TCL include flags
-# Have Makefile avoid Homebrew's install of tcl on Mac
-ifneq ($(OSTYPE), Darwin)
-TCL_INC_FLAGS = $(shell pkg-config --silence-errors --cflags-only-I tcl || echo -I/usr/include/tcl)
-endif
-
-# TCL library flags
-TCL_LIB_FLAGS = -ltcl$(TCL_VER)
 
 # -----
 # GHC

commit e330e11ed2b676d8853753ed1855eab796b0d444

Fix lookup of Tcl via pkg-config
Regression introduced by PR #427. The Tcl package may not have
a version suffix.

platform.sh
@@ -104,6 +104,10 @@ if [ "$1" = "tclinc" ] ; then
     if [ $? -ne 0 ] ; then
         TCL_INC_FLAGS=$(${PKG_CONFIG} --silence-errors --cflags-only-I tcl${TCL_ALT_SUFFIX})
     fi
+    # The tcl package may also not have a suffix at all, e.g. on Arch Linux.
+    if [ $? -ne 0 ] ; then
+        TCL_INC_FLAGS=$(${PKG_CONFIG} --silence-errors --cflags-only-I tcl)
+    fi
     # If pkg-config doesn't work, try some well-known locations
     if [ $? -ne 0 ] ; then
         if [ -f "/usr/local/include/tcl${TCL_SUFFIX}/tcl.h" ] ; then
@@ -139,6 +143,10 @@ if [ "$1" = "tcllibs" ] ; then
     # For example, on FreeBSD, the tcl87 package installs tclsh8.7, but tcl87.pc
     if [ $? -ne 0 ] ; then
         TCL_LIB_FLAGS=`${PKG_CONFIG} --silence-errors --libs tcl${TCL_ALT_SUFFIX}`
+    fi
+    # The tcl package may also not have a suffix at all, e.g. on Arch Linux.
+    if [ $? -ne 0 ] ; then
+        TCL_LIB_FLAGS=`${PKG_CONFIG} --silence-errors --libs tcl`
     fi
 
     if [ $? -eq 0 ] ; then