215e3ef5d3f027997b73b84656e70d274bcd45e2
Julie Schwartz <quark@bluespec.com>
2021-11-07 15:27:34 +1300

    Draft support for Verilator vsim

---
+++ src/Verilator/sim_main.cpp
@@ -0,0 +1,85 @@
+// Top-level driver for "verilated" objects (Verilog compiled with verilator)
+
+#include <verilated.h>
+
+#define Q(x) #x
+#define QUOTE(x) Q(x)
+#define APPEND(x,y) x ## y
+
+#define mkV(name) APPEND(V,name)
+
+#include QUOTE(mkV(TOP).h)
+
+// If "verilator --trace" is used, include the tracing class
+#if VM_TRACE
+# include <verilated_vcd_c.h>
+#endif
+
+vluint64_t main_time = 0;    // Current simulation time
+
+double sc_time_stamp () {    // Called by $time in Verilog
+    return main_time;
+}
+
+int main (int argc, char **argv, char **env) {
+    Verilated::commandArgs (argc, argv);    // remember args
+
+    mkV(TOP)* TOP = new mkV(TOP);    // create instance of model
+
+#if VM_TRACE
+    // If verilator was invoked with --trace argument,
+    // and if at run time passed the +bscvcd argument, turn on tracing
+    VerilatedVcdC* tfp = NULL;
+    const char* flag = Verilated::commandArgsPlusMatch("bscvcd");
+    if (flag && 0==strcmp(flag, "+bscvcd")) {
+        Verilated::traceEverOn(true);  // Verilator must compute traced signals
+        VL_PRINTF("Enabling waves into dump.vcd...\n");
+        tfp = new VerilatedVcdC;
+        TOP->trace(tfp, 99);  // Trace 99 levels of hierarchy
+        tfp->open("dump.vcd");  // Open the dump file
+    }
+#endif
+
+    // initial conditions in order to generate appropriate edges on
+    // reset
+    TOP->RST_N = 1;
+    TOP->CLK = 0;
+
+    while (! Verilated::gotFinish ()) {
+
+        if (main_time == 2) {
+            TOP->RST_N = 0;    // assert reset
+        }
+        else if (main_time == 7) {
+            TOP->RST_N = 1;    // Deassert reset
+        }
+
+        // Toggle clock
+        if ((main_time % 10) == 5) {
+            TOP->CLK = 1;
+        }
+        else if ((main_time % 10) == 0) {
+            TOP->CLK = 0;
+        }
+
+#if VM_TRACE
+        if (tfp)
+            tfp->dump(main_time);
+#endif
+
+        TOP->eval ();
+        main_time++;
+    }
+
+    TOP->final ();    // Done simulating
+
+    // Close trace if opened
+#if VM_TRACE
+    if (tfp) { tfp->close(); }
+#endif
+
+    delete TOP;
+    TOP = NULL;
+
+    exit (0);
+}
