c2fe1f364a71353d04872c80e9feb7637997d52a
Julie Schwartz <quark@bluespec.com>
2022-07-17 14:08:03 +1200

    GitHub CI: Don't hold back the macOS ccache version
    The version retrieved from Homebrew is now at a point that includes a
    fix for the bug that our testsuite was triggering.

--- .github/workflows/install_dependencies_macos.sh
+++ .github/workflows/install_dependencies_macos.sh
@@ -4,12 +4,7 @@
 # the build performance by caching C++ obj files across multiple builds.
 brew install \
   autoconf \
+  ccache \
   gperf \
   icarus-verilog \
   pkg-config
-
-# Hold ccache back to 4.3_1, to avoid bug introduced in 4.4
-TAP=b-lang/homebrew-old
-brew tap-new $TAP
-brew extract --version 4.3 homebrew/core/ccache $TAP
-brew install $TAP/ccache@4.3
--- .github/workflows/install_dependencies_testsuite_macos.sh
+++ .github/workflows/install_dependencies_testsuite_macos.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 brew install \
+  ccache \
   deja-gnu \
   icarus-verilog \
   systemc
-
-# Hold ccache back to 4.3_1, to avoid bug introduced in 4.4
-TAP=b-lang/homebrew-old
-brew tap-new $TAP
-brew extract --version 4.3 homebrew/core/ccache $TAP
-brew install $TAP/ccache@4.3
