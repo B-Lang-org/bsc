3683cf35d3f6f05528f87db5085ba985b7c6f7d0
Julie Schwartz <quark@bluespec.com>
2024-01-17 17:07:17 +1300

    GitHub CI: Include SystemC output in the archive on failure

--- testsuite/archive_logs.sh
+++ testsuite/archive_logs.sh
@@ -7,6 +7,7 @@ find . \
     -name '*.bsc-out' -o \
     -name '*.bsc-ccomp-out' -o \
     -name '*.bsc-vcomp-out' -o \
+    -name '*.cxx-comp-out' -o \
     -name '*.bsc-sched-out' |
     tar zcf logs.tar.gz -T -
 
