aaf9e9a21d24fb556f459c3e85b8868a5999880a
Lucas Kramer <lucas@matx.com>
2024-08-13 14:09:37 -0700

    Saving port types using WrapField type class method

--- src/comp/PreStrings.hs
+++ src/comp/PreStrings.hs
@@ -349,6 +349,7 @@ fsPolyWrapField    = mkFString "val"
 fsWrapField       = mkFString "WrapField"
 fsFromWrapField   = mkFString "fromWrapField"
 fsToWrapField     = mkFString "toWrapField"
+fsSaveFieldPortTypes = mkFString "saveFieldPortTypes"
 
 -- XXX low ASCII only, please...
 sAcute             = "__"
