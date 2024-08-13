fbee672452fd5964e89f448981161c6769f20785
Lucas Kramer <lucas@matx.com>
2024-08-12 19:47:53 -0700

    Input port splitting works end-to-end, modulo sanity checks and saving port types

--- src/comp/IfcBetterInfo.hs
+++ src/comp/IfcBetterInfo.hs
@@ -31,8 +31,7 @@ data BetterInfo = BetterMethodInfo
                     mi_result :: VPort, -- possible rename for method result
                     mi_ready  :: VPort, -- for ready signal
                     mi_enable :: VPort, -- for enable signal
-                    mi_prefix :: Id,    -- default prefix for arguments (which are not found in classic)
-                    mi_orig_type :: Maybe IType -- original (unwrapped) field type
+                    mi_prefix :: Id     -- default prefix for arguments (which are not found in classic)
                   }
                 -- XXX Note that the following are unused
                 -- XXX (this package needs re-thinking)
@@ -56,8 +55,7 @@ noMethodInfo fieldId = BetterMethodInfo {mi_id = fieldId,
                                      mi_result = id_to_vPort fieldId,
                                      mi_ready  = id_to_vPort $ mkRdyId fieldId,
                                      mi_enable = id_to_vPort $ mkEnableId fieldId,
-                                     mi_prefix = fieldId,
-                                     mi_orig_type = Nothing
+                                     mi_prefix = fieldId
                                   }
 
 
@@ -66,8 +64,7 @@ instance PPrint BetterInfo  where
                         ( printMaybe d i "Result:" (mi_result info) <>
                           printMaybe d i "Ready:" (mi_ready info) <>
                           printMaybe d i "Enable:" (mi_enable info) <>
-                          text "Prefix:" <> pPrint d i (mi_prefix info) <>
-                          printMaybe d i "Original type:" (mi_orig_type info)
+                          text "Prefix:" <> pPrint d i (mi_prefix info)
                         )
 
 printMaybe :: PPrint a => PDetail -> Int -> String -> a -> Doc
@@ -102,8 +99,7 @@ fieldInfoToBetterInfo flags symTab (fieldId, Just fi) =
                       mi_result = maybe (id_to_vPort fieldId) (str_to_vPort) mres,
                       mi_ready  = maybe (id_to_vPort $ mkRdyId fieldId) str_to_vPort mrdy,
                       mi_enable = maybe (id_to_vPort $ mkEnableId fieldId) str_to_vPort  men,
-                      mi_prefix = maybe fieldId (setIdBaseString fieldId) mprefix,
-                      mi_orig_type = fmap (iConvT flags symTab) (fi_orig_type fi)
+                      mi_prefix = maybe fieldId (setIdBaseString fieldId) mprefix
                }
     where prags   = fi_pragmas fi
           (mprefix,mres,mrdy,men,_,_,_) = getMethodPragmaInfo prags
