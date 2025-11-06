d176660bd1f222b17350b03baf8e1cc79c2dcd32
Ravi Nanavati <ravi_n@alum.mit.edu>
2025-11-05 18:20:56 -0800

    The use of unsafePeformIO when getting and setting the syntax mode means that setting the syntax mode after the first time it has been read doesn't reliably work.
    Track whether the syntax mode has been read and use that to determine if
    Bluetcl::syntax set succeeds or fails.

--- src/comp/Classic.hs
+++ src/comp/Classic.hs
@@ -1,6 +1,6 @@
 module Classic (SyntaxMode(..),
-                setSyntax,
-                isBSV, isClassic, isEse
+                setSyntax, isSyntaxFrozen,
+                isBSV, isClassic, isEse,
                ) where
 
 import IOMutVar(MutableVar, newVar, readVar, writeVar)
@@ -12,14 +12,27 @@ data SyntaxMode = BSV | CLASSIC | ESE
 syntax :: MutableVar SyntaxMode
 syntax = unsafePerformIO $ (newVar CLASSIC)
 
+syntaxFrozen :: MutableVar Bool
+syntaxFrozen = unsafePerformIO $ newVar False
+
+isSyntaxFrozen :: IO Bool
+isSyntaxFrozen = readVar syntaxFrozen
+
 setSyntax :: SyntaxMode -> IO ()
 setSyntax m = writeVar syntax m
 
-isBSV :: () -> Bool
-isBSV() = (unsafePerformIO $ readVar syntax) == BSV
+readSyntax :: IO SyntaxMode
+readSyntax = do
+  writeVar syntaxFrozen True
+  readVar syntax
 
+{-# NOINLINE isBSV #-}
+isBSV :: () -> Bool
+isBSV() = (unsafePerformIO readSyntax) == BSV
+
+{-# NOINLINE isClassic #-}
 isClassic :: () -> Bool
-isClassic() = (unsafePerformIO $ readVar syntax) == CLASSIC
+isClassic() = (unsafePerformIO readSyntax) == CLASSIC
 
 isEse :: () -> Bool
-isEse() = (unsafePerformIO $ readVar syntax) == ESE
+isEse() = (unsafePerformIO readSyntax) == ESE
