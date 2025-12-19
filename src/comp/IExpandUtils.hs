35b8268e5feb370e0f19e905ec107e38592bcb81
Ravi Nanavati <ravi_n@alum.mit.edu>
2025-12-18 22:51:04 -0800

    Deepseq IRefT expressions when created to prevent leaks.
    The strict evaluation means that they will not hold on to references
    to their source expression in thunks for calculating their type or
    position information (which would cause a leak).

--- src/comp/IExpandUtils.hs
+++ src/comp/IExpandUtils.hs
@@ -2463,7 +2463,9 @@ addHeapUnev tag t e cell_name = do
  when doTraceHeap $ traceM ("addHeapUnev " ++ ppString cell_name ++ " [" ++
                             prPositionConcise (getPosition cell_name) ++ "] " ++
                             ppReadable (result,t,e))
- return result -- (mapIExprPosition cross (e, result))
+ -- The deepseq forces the type and position in the IRefT so that
+ -- they will not be thunks holding references to the original expression.
+ return $ deepseq result result -- (mapIExprPosition cross (e, result))
 
 -- add an expression to the heap, noting it is WHNF
 addHeapWHNF :: String -> IType -> PExpr -> Maybe Id -> G HExpr
@@ -2475,7 +2477,10 @@ addHeapWHNF tag t pe@(P _ e) cell_name = do
   when doTraceHeap $ traceM ("addHeapWHNF " ++ ppString cell_name ++ " [" ++
                              prPositionConcise (getPosition cell_name) ++ "] " ++
                              ppReadable (result,t,pe))
-  return result
+
+  -- The deepseq forces the type and position in the IRefT so that
+  -- they will not be thunks holding references to the original expression.
+  return $ deepseq cell_name $ deepseq result result
 
 {-
 -- add an expression to the heap that is in NF
@@ -2487,7 +2492,7 @@ addHeapNF tag t pe ws = do
 -- flags <- getFlags
 -- mapIExprPosition flags?
   when doTraceHeap $ traceM ("addHeapNF " ++ ppReadable (result,t,pe))
-  return result
+  return $ deepseq result result
 -}
 
 -- "evalPred" needs to create references for shared expressions.
@@ -2505,7 +2510,7 @@ addHeapPred tag e = do
   let poss = S.singleton $ getIExprPosition e
   let result = IRefT t p poss r
   when doTraceHeap $ traceM ("addHeapPred " ++ ppReadable (result,e))
-  return result
+  return $ deepseq result result
 
 {-# INLINE getHeap #-}
 getHeap :: HeapData -> G HeapCell
