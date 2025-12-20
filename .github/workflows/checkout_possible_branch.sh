bbea72bcea3f3a95ebc1eb701223c421cbf39c9b
Julie Schwartz <quark@bluespec.com>
2025-12-20 13:42:26 +1300

    GitHub CI: Checkout other repos at same branch name if exists
    If a change to BSC requires a change in bsc-contrib (or BDW or Toooba),
    and the user has pushed commits to branches in both repos (perhaps also
    submitting a PR), the CI in each repo will fail until the other is merged.
    To allow testing both branches together, prior to merging, we look for a
    branch in the user's fork by the same name; if one exists, we clone that
    instead of the upstream 'main'.

---
+++ .github/workflows/checkout_possible_branch.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+OWNER=$1
+REPO=$2
+
+if [ -z "$OWNER" ] || [ -z "$REPO" ] ; then
+    echo "Usage: $0 <owner> <repo>"
+    exit 1;
+fi
+    
+default_checkout () {
+    echo "Checkout default"
+    git clone --recursive https://github.com/${OWNER}/${REPO} ../${REPO}
+}
+
+user_branch_checkout () {
+    if [ "$2" = "main" ]; then
+	default_checkout
+    else
+	CMD="git ls-remote --heads https://github.com/$1/${REPO} refs/heads/$2"
+	echo "Trying: $CMD"
+	SEARCH_RES=`$CMD`
+	echo "Result: ${SEARCH_RES}"
+	if [ -z "${SEARCH_RES}" ]; then
+            default_checkout
+	else
+            echo "Checking out user's branch"
+            git clone --recursive https://github.com/$1/${REPO} ../${REPO}
+            cd ../${REPO}
+            git checkout "$2"
+	fi
+    fi
+}
+
+if [ "${GITHUB_EVENT_NAME}" = "pull_request" ]; then
+    user_branch_checkout "${GITHUB_ACTOR}" "${GITHUB_HEAD_REF}"
+elif [ "${GITHUB_EVENT_NAME}" = "push" ]; then
+    user_branch_checkout "${GITHUB_ACTOR}" "${GITHUB_REF_NAME}"
+else
+    echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
+    default_checkout
+fi
