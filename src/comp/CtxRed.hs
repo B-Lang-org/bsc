2d7b7b427b102ddd6fc7dfc8072cdb4f302a6aba
Lucas Kramer <lucas@matx.com>
2026-04-13 12:58:51 -0700

    Fix #890 by expanding type functions in tiExpl
    Also adds tests including the ones added by @quark17 in #909

--- src/comp/CtxRed.hs
+++ src/comp/CtxRed.hs
@@ -385,10 +385,7 @@ ctxRedCQType' isInstHead cqt = do
     -- (and do it here, after "convCQType", so that "expTFun" sees
     -- the qualified types)
     (qs, t) <- if isInstHead
-               then do -- XXX disable expanding of type synonyms until
-                       -- XXX failures with TLM instances are resolved
-                       -- XXX (vqs_extra, t1) <- expTFun t0 (expandSyn t0)
-                       (vqs_extra, t1) <- expTFun t0
+               then do (vqs_extra, t1) <- expTFun (expandSyn t0)
                        let qs_extra = map toPredWithPositions vqs_extra
                        return (qs0 ++ qs_extra, t1)
                else return (qs0, t0)
